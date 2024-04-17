33acba02314b6f5ae733c5e0e0bf4f9604d14fda
gh-tayyab <muhammadtayyabmt0@gmail.com>
2024-04-17 12:50:02 +0500

    first commit

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -12,19 +12,15 @@ const Work = () => {
                 <div data-aos="fade-in" className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 py-4 w-auto px-4'>
                     <Link data-aos="fade-in" href={"https://flexforce-gymnasium.vercel.app/"} target='_main'>
                         <Image src="/website1.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
-                        <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                     </Link>
                     <Link data-aos="fade-in" href={"https://praroz.vercel.app/"} target='_main'>
                         <Image src="/website2.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
-                        <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                     </Link>
                     <Link data-aos="fade-in" href={"https://seaside-escape-gateways.vercel.app/"} target='_main'>
                         <Image src="/website3.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
-                        <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-purple-800 text-neutral-400 text-8xl' />
                     </Link>
                     <Link data-aos="fade-in" href={"https://sportswebsite.vercel.app/"} target='_main'>
                         <Image src="/website4.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
-                        <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                     </Link>
 
                 </div>
