deb4f0fad55e21fef0a095c770f45a7aa62d51c2
gh-tayyab <muhammadtayyabmt0@gmail.com>
2024-04-17 13:07:37 +0500

    first commit

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -10,17 +10,17 @@ const Work = () => {
                 <h1 data-aos="fade-in" className='text-5xl lg:text-6xl font-bold text-center py-6 text-[#34ebde]'>Projects</h1>
 
                 <div data-aos="fade-in" className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 py-4 w-auto px-4'>
-                    <Link data-aos="fade-in" href={"https://flexforce-gymnasium.vercel.app/"} target='_main'>
-                        <Image src="/website1.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
+                    <Link data-aos="fade-in" href={"https://flexforce-gymnasium.vercel.app/"} target='_main' title='FlexForce-Gymnasium Website'>
+                        <Image src="/website1.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 transition duration-300 ease-in-out transform' />
                     </Link>
-                    <Link data-aos="fade-in" href={"https://praroz.vercel.app/"} target='_main'>
-                        <Image src="/website2.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
+                    <Link data-aos="fade-in" href={"https://praroz.vercel.app/"} target='_main' title='PraRoz Website'>
+                        <Image src="/website2.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 transition duration-300 ease-in-out transform' />
                     </Link>
-                    <Link data-aos="fade-in" href={"https://seaside-escape-gateways.vercel.app/"} target='_main'>
-                        <Image src="/website3.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
+                    <Link data-aos="fade-in" href={"https://seaside-escape-gateways.vercel.app/"} target='_main' title='SeaSide_Escape-Gateways Website'>
+                        <Image src="/website3.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 transition duration-300 ease-in-out transform' />
                     </Link>
-                    <Link data-aos="fade-in" href={"https://sportswebsite.vercel.app/"} target='_main'>
-                        <Image src="/website4.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform' />
+                    <Link data-aos="fade-in" href={"https://sportswebsite.vercel.app/"} target='_main' title='Sports Website'>
+                        <Image src="/website4.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 transition duration-300 ease-in-out transform' />
                     </Link>
 
                 </div>
