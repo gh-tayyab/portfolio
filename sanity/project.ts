3310b6cc2f4fa6dd9808fd204c86fefa7747c24e
gh-tayyab <muhammadtayyabmt0@gmail.com>
2024-05-10 03:15:53 -0700

    first commit

---
+++ sanity/project.ts
@@ -0,0 +1,27 @@
+
+export const project=
+    {
+        name: 'project',
+        type: 'document',
+        title: 'Product',
+        fields: [
+            
+            {
+                name: 'title',
+                title: 'Title',
+                type: 'string',
+              },
+              
+            {
+                name: 'image',
+                type: 'image',
+                title: 'Product  Image'
+            },
+            {
+                name: 'link',
+                title: 'Link',
+                type: 'url',
+              }
+        ]
+    
+    }
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,37 +1,36 @@
 import React from 'react'
 import Link from 'next/link'
-import { BsEye } from 'react-icons/bs'
 import Image from 'next/image'
-
-const Work = () => {
+import {client} from '@/lib/sanityClient'
+import { Image as IImage} from 'sanity'
+import { urlForImage } from '../../../sanity/lib/image'
+export const getProductdata=async()=>{
+    const res = await client.fetch(`*[_type=='product']{title, image, link}`)
+    return res
+ }
+ interface IProp{
+     title:string
+     image:IImage,
+     link:string
+ }
+ 
+const Work =async () => {
+    const data:IProp[] =  await getProductdata()
     return (
         <div className='flex items-center justify-center bg-neutral-950'>
             <div>
                 <h1 data-aos="fade-in" className='text-5xl lg:text-6xl font-bold text-center py-6 text-[#34ebde]'>Projects</h1>
 
                 <div data-aos="fade-in" className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 py-4 w-auto px-4'>
-                    <Link data-aos="fade-in" href={"https://flexforce-gymnasium.vercel.app/"} target='_main' title='FlexForce-Gymnasium Website'>
-                        <Image src={"/website1.jpg"} alt='Flex Force Website' width={600} height={600} className='hover:scale-105 duration-300' /> 
-                    </Link>
-                   
-                   
-                   
-                    <Link data-aos="fade-in" href={"https://praroz.vercel.app/"} target='_main' title='PraRoz Website'>
-                        <Image src={"/website2.jpg"} alt='PraRoz Website' width={600} height={600} className='hover:scale-105 duration-300'/>
-                    </Link>
-                   
-                   
-                   
-                    <Link data-aos="fade-in" href={"https://seaside-escape-gateways.vercel.app/"} target='_main' title='SeaSide_Escape-Gateways Website'>
-                        <Image src={"/website3.jpg"} alt='SeaSide Escape Gateways Website' width={600} height={600} className='hover:scale-105 duration-300'/> 
-                    </Link>
-                    
-                    
-                    
-                    <Link data-aos="fade-in" href={"https://sportswebsite.vercel.app/"} target='_main' title='Sports Website'>
-                        <Image src={"/website4.jpg"} alt='Sports Website' width={600} height={600} className='hover:scale-105 duration-300'/>
-                    </Link>
-
+                    {data.map((item:IProp,i:number)=>{
+                        return(
+                            <Link data-aos="fade-in" href={item.link} target='_main' title='FlexForce-Gymnasium Website' key={i}>
+                            <Image src={urlForImage(item.image)} alt='Flex Force Website' width={600} height={600} className='hover:scale-105 duration-300' /> 
+                            </Link>  
+                            
+                        )
+                    })}
+                
                 </div>
             </div>
 
@@ -40,3 +39,24 @@ const Work = () => {
 }
 
 export default Work
+{/*
+{/* 
+
+
+
+<Link data-aos="fade-in" href={"https://praroz.vercel.app/"} target='_main' title='PraRoz Website'>
+<Image src={"/website2.jpg"} alt='PraRoz Website' width={600} height={600} className='hover:scale-105 duration-300'/>
+</Link>
+
+
+
+<Link data-aos="fade-in" href={"https://seaside-escape-gateways.vercel.app/"} target='_main' title='SeaSide_Escape-Gateways Website'>
+<Image src={"/website3.jpg"} alt='SeaSide Escape Gateways Website' width={600} height={600} className='hover:scale-105 duration-300'/> 
+</Link>
+
+
+
+<Link data-aos="fade-in" href={"https://sportswebsite.vercel.app/"} target='_main' title='Sports Website'>
+<Image src={"/website4.jpg"} alt='Sports Website' width={600} height={600} className='hover:scale-105 duration-300'/>
+</Link> */}
+
